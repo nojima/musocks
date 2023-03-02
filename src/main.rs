d42e19db473eda14bdd0b5e420734362895366d4
Yusuke Nojima <nojima@ynojima.com>
2023-03-03 01:49:44 +0900

    Add logging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+use slog::Drain;
+
 mod socks;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    socks::serve().await
+    let logger = setup_logger();
+    let server = socks::Server {
+        logger: logger.clone(),
+    };
+    server.serve().await
+}
+
+fn setup_logger() -> slog::Logger {
+    let decorator = slog_term::PlainSyncDecorator::new(std::io::stderr());
+    let drain = slog_term::FullFormat::new(decorator).build().fuse();
+    slog::Logger::root(drain, slog::o!())
 }
--- src/socks.rs
+++ src/socks.rs
@@ -1,6 +1,11 @@
-use std::{io, net::Ipv4Addr};
+use std::{
+    io,
+    net::{Ipv4Addr, SocketAddr},
+    time::Instant,
+};
 
 use anyhow::bail;
+use slog::{info, o};
 use tokio::{
     io::{
         AsyncBufRead, AsyncBufReadExt, AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt,
@@ -13,111 +18,153 @@ struct Request {
     upstream_addr: (Ipv4Addr, u16),
 }
 
-pub async fn serve() -> anyhow::Result<()> {
-    let listener = TcpListener::bind("0.0.0.0:1080")
-        .await
-        .map_err(|e| anyhow::anyhow!("failed to bind: {e}"))?;
-    loop {
-        match listener.accept().await {
-            Ok((conn, _)) => {
-                tokio::spawn(handle_conn(conn));
-            }
-            Err(err) => {
-                eprintln!("ERROR: {err}");
+pub struct Server {
+    pub logger: slog::Logger,
+}
+
+impl Server {
+    pub async fn serve(self) -> anyhow::Result<()> {
+        let port = 1080;
+        let listener = TcpListener::bind(("0.0.0.0", port))
+            .await
+            .map_err(|e| anyhow::anyhow!("failed to bind: {e}"))?;
+        info!(self.logger, "server started"; "port" => port);
+
+        let mut conn_id = 0;
+        loop {
+            conn_id += 1;
+            match listener.accept().await {
+                Ok((conn, addr)) => {
+                    let h = Handler {
+                        logger: self.logger.new(o!("id" => conn_id)),
+                    };
+                    tokio::spawn(h.handle(conn, addr));
+                }
+                Err(err) => {
+                    slog::error!(self.logger, "failed to accept"; "err" => %err);
+                }
             }
         }
     }
 }
 
-async fn handle_conn(mut client: TcpStream) -> anyhow::Result<()> {
-    let mut preamble: [u8; 2] = Default::default();
-    client.read_exact(&mut preamble).await?;
+struct Handler {
+    logger: slog::Logger,
+}
 
-    let version = preamble[0];
-    let upstream = match version {
-        0x4 => {
-            let mut reader = BufReader::new(&mut client);
-            handle_socks4(&mut reader, preamble[1]).await?
+impl Handler {
+    async fn handle(self, client: TcpStream, client_addr: SocketAddr) {
+        if let Err(e) = self.handle_conn(client, client_addr).await {
+            slog::error!(self.logger, "proxy failed"; "err" => %e);
         }
-        _ => bail!("unsupported SOCKS version: {}", version),
-    };
-
-    let (client_reader, client_writer) = client.into_split();
-    let (upstream_reader, upstream_writer) = upstream.into_split();
-    do_proxy(
-        client_reader,
-        client_writer,
-        upstream_reader,
-        upstream_writer,
-    )
-    .await?;
-
-    Ok(())
-}
-
-async fn handle_socks4(
-    client: &mut (impl AsyncBufRead + AsyncWrite + Unpin),
-    cmd: u8,
-) -> anyhow::Result<TcpStream> {
-    let request = read_connect_request(client, cmd).await?;
-    return_reply(client).await?;
-    connect_to_upstream(request.upstream_addr).await
-}
-
-async fn read_connect_request(
-    client: &mut (impl AsyncBufRead + Unpin),
-    cmd: u8,
-) -> anyhow::Result<Request> {
-    let mut buf: [u8; 2 + 4] = Default::default();
-    client.read_exact(&mut buf).await?;
-
-    if cmd != 0x1 {
-        anyhow::bail!("Only CONNECT command is supported")
     }
-    let dst_port = u16::from_be_bytes([buf[0], buf[1]]);
-    let dst_addr = Ipv4Addr::new(buf[2], buf[3], buf[4], buf[5]);
 
-    let mut ident = Vec::new();
-    client.read_until(0, &mut ident).await?;
+    async fn handle_conn(
+        &self,
+        mut client: TcpStream,
+        client_addr: SocketAddr,
+    ) -> anyhow::Result<()> {
+        let started_at = Instant::now();
+        info!(self.logger, "proxy start"; "client_addr" => client_addr);
 
-    Ok(Request {
-        upstream_addr: (dst_addr, dst_port),
-    })
-}
+        let mut preamble: [u8; 2] = Default::default();
+        client.read_exact(&mut preamble).await?;
 
-async fn return_reply(client: &mut (impl AsyncWrite + Unpin)) -> io::Result<()> {
-    let buf: [u8; 1 + 1 + 2 + 4] = [
-        0,    // VN
-        0x5A, // REP
-        0, 0, // DSTPORT
-        0, 0, 0, 0, // DSTIP
-    ];
-    client.write_all(&buf).await
-}
+        let version = preamble[0];
+        let upstream = match version {
+            0x4 => {
+                let mut reader = BufReader::new(&mut client);
+                self.handle_socks4(&mut reader, preamble[1]).await?
+            }
+            _ => bail!("unsupported SOCKS version: {}", version),
+        };
 
-async fn connect_to_upstream(addr: (Ipv4Addr, u16)) -> anyhow::Result<TcpStream> {
-    TcpStream::connect(addr)
-        .await
-        .map_err(|e| anyhow::anyhow!("failed to connect to upstream: {}", e.to_string()))
-}
+        let (client_reader, client_writer) = client.into_split();
+        let (upstream_reader, upstream_writer) = upstream.into_split();
+        let (uploaded_bytes, downloaded_bytes) = self
+            .do_proxy(
+                client_reader,
+                client_writer,
+                upstream_reader,
+                upstream_writer,
+            )
+            .await?;
 
-async fn do_proxy(
-    client_reader: impl AsyncRead + Unpin,
-    client_writer: impl AsyncWrite + Unpin,
-    upstream_reader: impl AsyncRead + Unpin,
-    upstream_writer: impl AsyncWrite + Unpin,
-) -> io::Result<()> {
-    tokio::try_join!(
-        copy_and_drop(client_reader, upstream_writer),
-        copy_and_drop(upstream_reader, client_writer),
-    )?;
-    Ok(())
-}
+        let elapsed = started_at.elapsed();
+        info!(self.logger, "proxy done";
+            "downloaded_bytes" => downloaded_bytes,
+            "uploaded_bytes" => uploaded_bytes,
+            "elapsed" => ?elapsed,
+        );
+        Ok(())
+    }
 
-async fn copy_and_drop(
-    mut reader: impl AsyncRead + Unpin,
-    mut writer: impl AsyncWrite + Unpin,
-) -> io::Result<()> {
-    tokio::io::copy(&mut reader, &mut writer).await?;
-    Ok(())
+    async fn handle_socks4(
+        &self,
+        client: &mut (impl AsyncBufRead + AsyncWrite + Unpin),
+        cmd: u8,
+    ) -> anyhow::Result<TcpStream> {
+        let request = self.read_connect_request(client, cmd).await?;
+        self.return_reply(client).await?;
+        self.connect_to_upstream(request.upstream_addr).await
+    }
+
+    async fn read_connect_request(
+        &self,
+        client: &mut (impl AsyncBufRead + Unpin),
+        cmd: u8,
+    ) -> anyhow::Result<Request> {
+        let mut buf: [u8; 2 + 4] = Default::default();
+        client.read_exact(&mut buf).await?;
+
+        if cmd != 0x1 {
+            anyhow::bail!("Only CONNECT command is supported")
+        }
+        let dst_port = u16::from_be_bytes([buf[0], buf[1]]);
+        let dst_addr = Ipv4Addr::new(buf[2], buf[3], buf[4], buf[5]);
+
+        let mut ident = Vec::new();
+        client.read_until(0, &mut ident).await?;
+
+        Ok(Request {
+            upstream_addr: (dst_addr, dst_port),
+        })
+    }
+
+    async fn return_reply(&self, client: &mut (impl AsyncWrite + Unpin)) -> io::Result<()> {
+        let buf: [u8; 1 + 1 + 2 + 4] = [
+            0,    // VN
+            0x5A, // REP
+            0, 0, // DSTPORT
+            0, 0, 0, 0, // DSTIP
+        ];
+        client.write_all(&buf).await
+    }
+
+    async fn connect_to_upstream(&self, addr: (Ipv4Addr, u16)) -> anyhow::Result<TcpStream> {
+        TcpStream::connect(addr)
+            .await
+            .map_err(|e| anyhow::anyhow!("failed to connect to upstream: {}", e.to_string()))
+    }
+
+    async fn do_proxy(
+        &self,
+        client_reader: impl AsyncRead + Unpin,
+        client_writer: impl AsyncWrite + Unpin,
+        upstream_reader: impl AsyncRead + Unpin,
+        upstream_writer: impl AsyncWrite + Unpin,
+    ) -> io::Result<(u64, u64)> {
+        tokio::try_join!(
+            self.copy_and_drop(client_reader, upstream_writer),
+            self.copy_and_drop(upstream_reader, client_writer),
+        )
+    }
+
+    async fn copy_and_drop(
+        &self,
+        mut reader: impl AsyncRead + Unpin,
+        mut writer: impl AsyncWrite + Unpin,
+    ) -> io::Result<u64> {
+        tokio::io::copy(&mut reader, &mut writer).await
+    }
 }
